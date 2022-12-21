649d9eddef6b47a5881dc249aba77abb7f54f652
pierremolinaro <pierre@pcmolinaro.name>
2022-12-21 12:35:09 +0100

    Ajout choix format package / application

--- build-rss-xml.swift
+++ build-rss-xml.swift
@@ -165,6 +165,7 @@ struct VersionDescriptor : Codable {
   var news = [String] ()
   var changes = [String] ()
   var date = ""
+  var contents = ""
   var osmin = ""
 }
 
@@ -227,7 +228,7 @@ for (major, minor, patch) in sortedReleases {
     exit (1)
   }
 //--- Add build date
-  let buildDate = ISO8601DateFormatter ().date(from: versionDescriptor.date)!
+  let buildDate = ISO8601DateFormatter ().date (from: versionDescriptor.date)!
   let formatter = DateFormatter()
   formatter.locale = Locale(identifier: "en_US_POSIX")
   formatter.timeZone = TimeZone(secondsFromGMT: 0)
@@ -239,12 +240,12 @@ for (major, minor, patch) in sortedReleases {
 //--- enclosure
   let enclosure = XMLElement (name: "enclosure")
   let url = "https://raw.githubusercontent.com/pierremolinaro/ElCanari-distribution/master/ElCanari-\(version).dmg"
-  enclosure.addAttribute (XMLNode.attribute (withName: "url", stringValue:url) as! XMLNode)
-  enclosure.addAttribute (XMLNode.attribute (withName: "type", stringValue:"application/octet-stream") as! XMLNode)
-  enclosure.addAttribute (XMLNode.attribute (withName: "sparkle:edSignature", stringValue:versionDescriptor.edSignature) as! XMLNode)
-  enclosure.addAttribute (XMLNode.attribute (withName: "sparkle:version", stringValue:version) as! XMLNode)
-  enclosure.addAttribute (XMLNode.attribute (withName: "length", stringValue:"\(dmgFileLength)") as! XMLNode)
-  enclosure.addAttribute (XMLNode.attribute (withName: "sparkle:installationType", stringValue:"package") as! XMLNode)
+  enclosure.addAttribute (XMLNode.attribute (withName: "url", stringValue: url) as! XMLNode)
+  enclosure.addAttribute (XMLNode.attribute (withName: "type", stringValue: "application/octet-stream") as! XMLNode)
+  enclosure.addAttribute (XMLNode.attribute (withName: "sparkle:edSignature", stringValue: versionDescriptor.edSignature) as! XMLNode)
+  enclosure.addAttribute (XMLNode.attribute (withName: "sparkle:version", stringValue: version) as! XMLNode)
+  enclosure.addAttribute (XMLNode.attribute (withName: "length", stringValue: "\(dmgFileLength)") as! XMLNode)
+  enclosure.addAttribute (XMLNode.attribute (withName: "sparkle:installationType", stringValue: versionDescriptor.contents) as! XMLNode)
   item.addChild (enclosure)
 //--- Description
   let description = XMLElement (name: "description")
